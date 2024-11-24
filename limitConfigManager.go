79b2280da8bf70e0ff4817806199398ebf6b9366
Patrick H <hellopatrickhowell@gmail.com>
2024-11-24 15:46:16 -0500

    WIP

--- limitConfigManager.go
+++ limitConfigManager.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	pb "slowbro/internal/rateLimiter"
+	"strconv"
 
 	"github.com/redis/go-redis/v9"
 )
 
-const DefaultConfigDBInstace int = 0
+const (
+	defaultConfigDBInstace int = 0
+	maxConfigUpdateRetries int = 1000
+)
 
 type EndpointLimitConfig struct {
 	TokenLimit float64 `redis:"token_limit"`
@@ -16,21 +23,114 @@ type EndpointLimitConfig struct {
 
 type EndpointConfigManager struct {
 	client    *redis.Client
-	endpoints map[string]EndpointLimitConfig
+	endpoints map[string]*EndpointLimitConfig
+}
+
+func decodeConfig(data map[string]string) (*EndpointLimitConfig, error) {
+	config := &EndpointLimitConfig{}
+
+	// Parse float values
+	limit, err := strconv.ParseFloat(data["token_limit"], 64)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse limit: %v", err)
+	}
+	config.TokenLimit = limit
+
+	refill, err := strconv.ParseFloat(data["refill_rate"], 64)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse refill rate: %v", err)
+	}
+	config.RefillRate = refill
+
+	version, err := strconv.Atoi(data["version"])
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse version: %v", err)
+	}
+	config.Version = version
+
+	return config, nil
+}
+
+func (config *EndpointLimitConfig) toRedisMap() map[string]interface{} {
+	return map[string]interface{}{
+		"token_limit": config.TokenLimit,
+		"refill_rate": config.RefillRate,
+		"version":     config.Version,
+	}
 }
 
 func NewEndpointConfigManager(dbPath string) *EndpointConfigManager {
 	mgr := EndpointConfigManager{
 		client: redis.NewClient(&redis.Options{
 			Addr: dbPath,
-			DB:   DefaultConfigDBInstace,
+			DB:   defaultConfigDBInstace,
 		}),
-		endpoints: make(map[string]EndpointLimitConfig),
+		endpoints: make(map[string]*EndpointLimitConfig),
 	}
 
 	return &mgr
 }
 
-func (cfg *EndpointConfigManager) UpdateOrAddEndpoint(req *pb.RateLimitEndpointPerUserConfig) error {
+func getEndpointKey(req *pb.RateLimitEndpointPerUserConfig) string {
+	return fmt.Sprintf("%s:%s", req.ServiceName, req.Endpoint)
+}
 
+func (mgr *EndpointConfigManager) UpdateOrAddEndpoint(req *pb.RateLimitEndpointPerUserConfig) error {
+	key := getEndpointKey(req)
+
+	var err error
+
+	for i := 0; i < maxConfigUpdateRetries; i++ {
+		err = mgr.client.Watch(context.Background(), func(tx *redis.Tx) error {
+			// Retrieve or create config
+			configData, err := tx.HGetAll(context.Background(), key).Result()
+			if err != nil {
+				log.Printf("[ERROR] Redis HGetAll failed for key: %v", key)
+				return err
+			}
+
+			var config *EndpointLimitConfig
+			if len(configData) == 0 {
+				log.Printf("[INFO] Creating new config for key: %v", key)
+				config = &EndpointLimitConfig{
+					TokenLimit: float64(req.MaxTokens),
+					RefillRate: float64(req.TokenFillRate),
+					Version:    0,
+				}
+			} else {
+				config, err = decodeConfig(configData)
+				if err != nil {
+					log.Printf("[ERROR] Failed to decode config for key: %s, error: %v", key, err)
+					return err
+				}
+				log.Printf("[DEBUG] Retrieved config for key: %s", key)
+			}
+
+			// Update the configuration
+			config.RefillRate = float64(req.TokenFillRate)
+			config.TokenLimit = float64(req.MaxTokens)
+			config.Version++
+
+			// Attempt to update the database
+			_, err = tx.TxPipelined(context.Background(), func(pipe redis.Pipeliner) error {
+				err = pipe.HSet(context.Background(), key, config.toRedisMap()).Err()
+				if err != nil {
+					return err
+				}
+
+				// Success, update the in-memory struct
+				mgr.endpoints[key] = config
+				return nil
+			})
+
+			if err != nil {
+				log.Printf("[ERROR] Failed to update config for key: %s", key)
+				return err
+			}
+
+			return nil
+		}, key)
+	}
+
+	return err
 }

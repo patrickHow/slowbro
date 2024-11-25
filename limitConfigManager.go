67e8dcbbcfc223a4eea8e78580df918089f83adc
Patrick H <pah408@gmail.com>
2024-11-25 11:01:59 -0500

    Initial version of pubsub between instances

--- limitConfigManager.go
+++ limitConfigManager.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	pb "slowbro/internal/rateLimiter"
 	"strconv"
+	"sync"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -22,30 +24,41 @@ type EndpointLimitConfig struct {
 	Version    int     `redis:"version"`
 }
 
-type EndpointConfigManager struct {
-	client    *redis.Client
-	endpoints map[string]*EndpointLimitConfig
+// Struct for receiving and sending limit configs on the redis pubsub channel
+// JSON encoded since the message payload is just a string
+type EndpointConfigMessage struct {
+	Key        string  `json:"key"`
+	TokenLimit float64 `json:"token_limit"`
+	RefillRate float64 `json:"refill_rate"`
+	Version    int     `json:"version"`
 }
 
-func decodeConfig(data map[string]string) (*EndpointLimitConfig, error) {
-	config := &EndpointLimitConfig{}
+type EndpointConfigManager struct {
+	client    *redis.Client
+	channel   *redis.PubSub
+	lock      *sync.Mutex // Locks access to endpoint map
+	endpoints map[string]EndpointLimitConfig
+}
+
+func decodeConfig(data map[string]string) (EndpointLimitConfig, error) {
+	config := EndpointLimitConfig{}
 
 	// Parse float values
 	limit, err := strconv.ParseFloat(data["token_limit"], 64)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse limit: %v", err)
+		return EndpointLimitConfig{}, fmt.Errorf("failed to parse limit: %v", err)
 	}
 	config.TokenLimit = limit
 
 	refill, err := strconv.ParseFloat(data["refill_rate"], 64)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse refill rate: %v", err)
+		return EndpointLimitConfig{}, fmt.Errorf("failed to parse refill rate: %v", err)
 	}
 	config.RefillRate = refill
 
 	version, err := strconv.Atoi(data["version"])
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse version: %v", err)
+		return EndpointLimitConfig{}, fmt.Errorf("failed to parse version: %v", err)
 	}
 	config.Version = version
 
@@ -66,10 +79,44 @@ func NewEndpointConfigManager(dbPath string) *EndpointConfigManager {
 			Addr: dbPath,
 			DB:   defaultConfigDBInstace,
 		}),
-		endpoints: make(map[string]*EndpointLimitConfig),
+		lock:      &sync.Mutex{},
+		endpoints: make(map[string]EndpointLimitConfig),
 	}
 
+	// Subscribe to the config update channel
+	mgr.channel = mgr.client.Subscribe(context.Background(), configUpdateChannel)
+
 	return &mgr
+}
+
+func (mgr *EndpointConfigManager) ReceiveConfigUpdates() {
+	ch := mgr.channel.Channel()
+	for {
+		select {
+		case msg, ok := <-ch:
+			if !ok {
+				// Channel has closed
+				log.Printf("[INFO] Redis pubsub channel closed")
+				return
+			}
+			// Update the manager's in-memory struct
+			var config EndpointConfigMessage
+			err := json.Unmarshal([]byte(msg.Payload), &config)
+			if err != nil {
+				log.Printf("[ERROR] Error decoding json from pubsub: %v", err)
+			} else {
+				// If unmarshal was succesful, update the structure in memory
+				mgr.lock.Lock()
+				mgr.endpoints[config.Key] = EndpointLimitConfig{
+					TokenLimit: config.TokenLimit,
+					RefillRate: config.RefillRate,
+					Version:    config.Version,
+				}
+				mgr.lock.Unlock()
+				log.Printf("[INFO] Got config update for endpoint: %s", config.Key)
+			}
+		}
+	}
 }
 
 func getEndpointKey(req *pb.RateLimitEndpointPerUserConfig) string {
@@ -104,10 +151,10 @@ func (mgr *EndpointConfigManager) tryUpdate(req *pb.RateLimitEndpointPerUserConf
 			return err
 		}
 
-		var config *EndpointLimitConfig
+		var config EndpointLimitConfig
 		if len(configData) == 0 {
 			log.Printf("[INFO] Creating new config for key: %v", key)
-			config = &EndpointLimitConfig{
+			config = EndpointLimitConfig{
 				TokenLimit: float64(req.MaxTokens),
 				RefillRate: float64(req.TokenFillRate),
 				Version:    0,
@@ -134,8 +181,30 @@ func (mgr *EndpointConfigManager) tryUpdate(req *pb.RateLimitEndpointPerUserConf
 			}
 
 			// Success, update the in-memory struct
+			mgr.lock.Lock()
 			mgr.endpoints[key] = config
-			return nil
+			mgr.lock.Unlock()
+
+			// No error during update, publish changes
+			msg := EndpointConfigMessage{
+				TokenLimit: config.TokenLimit,
+				RefillRate: config.RefillRate,
+				Version:    config.Version,
+				Key:        key,
+			}
+			bytes, err := json.Marshal(&msg)
+			if err != nil {
+				log.Printf("[ERROR] Error marshalling json for update publish: %v", err)
+				return err
+			}
+			err = mgr.client.Publish(context.Background(), configUpdateChannel, bytes).Err()
+			if err != nil {
+				log.Printf("[ERROR] Error sending new config data to pubsub: %v", err)
+			} else {
+				log.Printf("[INFO] Publishing config update for endpoint: %s", key)
+			}
+
+			return err
 		})
 
 		if err != nil {
@@ -149,12 +218,12 @@ func (mgr *EndpointConfigManager) tryUpdate(req *pb.RateLimitEndpointPerUserConf
 	return err
 }
 
-func (mgr *EndpointConfigManager) GetEndpointConfig(key string) *EndpointLimitConfig {
+func (mgr *EndpointConfigManager) GetEndpointConfig(key string) EndpointLimitConfig {
 	config, ok := mgr.endpoints[key]
 
 	if !ok {
 		// Config not found, return a default struct
-		return &EndpointLimitConfig{
+		return EndpointLimitConfig{
 			TokenLimit: 0.0,
 			RefillRate: 0.0,
 			Version:    0,
